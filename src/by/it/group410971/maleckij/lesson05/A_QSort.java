4232b523cbef4e539cf5a07f53bd1210de6df611
Александр <amaleckij197@gmail.com>
2025-06-12 19:37:17 +0300

    Выполнение lesson5 А

---
+++ src/by/it/group410971/maleckij/lesson05/A_QSort.java
@@ -0,0 +1,67 @@
+package by.it.group410971.maleckij.lesson05;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A_QSort {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = A_QSort.class.getResourceAsStream("dataA.txt");
+        A_QSort instance = new A_QSort();
+        int[] result = instance.getAccessory(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+
+    int[] getAccessory(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+        int n = scanner.nextInt();
+        Segment[] segments = new Segment[n];
+        int m = scanner.nextInt();
+        int[] points = new int[m];
+        int[] result = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+        }
+        for (int i = 0; i < m; i++) {
+            points[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(segments);
+
+        for (int i = 0; i < m; i++) {
+            int point = points[i];
+            int count = 0;
+            for (Segment segment : segments) {
+                if (segment.start <= point && point <= segment.stop) {
+                    count++;
+                }
+                if (segment.start > point) {
+                    break;
+                }
+            }
+            result[i] = count;
+        }
+
+        return result;
+    }
+
+    private class Segment implements Comparable<Segment> {
+        int start;
+        int stop;
+
+        Segment(int start, int stop) {
+            this.start = Math.min(start, stop);
+            this.stop = Math.max(start, stop);
+        }
+
+        @Override
+        public int compareTo(Segment o) {
+            return Integer.compare(this.start, o.start);
+        }
+    }
+}
